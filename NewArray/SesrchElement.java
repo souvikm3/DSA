cec71c8e6ed20ca6c6aee1ba3c1f1360fee7ab28
Souvik Maiti <souvikmaiti391@gmail.com>
2023-06-26 17:13:25 +0530

    Change 2D Array

--- NewArray/SesrchElement.java
+++ NewArray/SesrchElement.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class SesrchElement {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of Rows: ");
         int row=sc.nextInt();
+        System.out.println("Enter the number of Columns: ");
         int column=sc.nextInt();
         int arr[][]=new int[row][column];
         for(int i=0;i<row;i++){
             for(int j=0;j<column;j++){
+                 System.out.println("Enter the "+i+" row and "+j+"th column");
                 arr[i][j]=sc.nextInt();
             }
         }
