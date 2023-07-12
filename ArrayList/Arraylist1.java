c2a69c0e5b81f15d1b3ef4311853e47bda1b8fea
Souvik Maiti <souvikmaiti391@gmail.com>
2023-07-12 17:53:49 +0530

    ARRAYLIST UPDATE

---
+++ ArrayList/Arraylist1.java
@@ -0,0 +1,7 @@
+package ArrayList;
+import java.util.ArrayList;
+public class Arraylist1 {
+    public static void main(String[] args) {
+        ArrayList<Integer>list1=new ArrayList<>();
+    }
+}
