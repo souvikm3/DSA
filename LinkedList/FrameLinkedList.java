c6838ebb9888aa4bff8255954acd6fa42802f481
Souvik Maiti <souvikmaiti391@gmail.com>
2023-07-27 00:47:36 +0530

    LinkedList Upadte Part 2

---
+++ LinkedList/FrameLinkedList.java
@@ -0,0 +1,14 @@
+package LinkedList;
+import java.util.LinkedList;;
+public class FrameLinkedList {
+    public static void main(String[] args) {
+        //Create
+        LinkedList<Integer>ll=new LinkedList<>();
+        //Add
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addFirst(1);
+        
+        //Remove
+    }
+}
